c7f25201d3298875c82c0eab20a5aacfadd37cbb
bond <bond00728tm@gmail.com>
2017-12-20 20:18:48 +0900

    WIN32_FIND_DATAW用初期化関数追加

--- src/minwinbase.rs
+++ src/minwinbase.rs
@@ -17,3 +17,19 @@ pub fn WIN32_FIND_DATAA_New() -> WIN32_FIND_DATAA{
         cAlternateFileName: [0; 14]
     }
 }
+
+pub fn WIN32_FIND_DATAW_New() -> WIN32_FIND_DATAW{
+    
+    WIN32_FIND_DATAW{
+        dwFileAttributes: 0,
+        ftCreationTime: FILETIME_New(),
+        ftLastAccessTime: FILETIME_New(),
+        ftLastWriteTime: FILETIME_New(),
+        nFileSizeHigh: 0,
+        nFileSizeLow: 0,
+        dwReserved0: 0,
+        dwReserved1: 0,
+        cFileName: [0; MAX_PATH],
+        cAlternateFileName: [0; 14]
+    }
+}
